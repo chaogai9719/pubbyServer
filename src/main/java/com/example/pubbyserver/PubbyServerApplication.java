f4acb3d9210b7e899db34a42721a881d52ebbd3c
chaogai9719 <chdcfp@163.com>
2025-11-15 19:59:29 +0800

    项目初始化

---
+++ src/main/java/com/example/pubbyserver/PubbyServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pubbyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubbyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PubbyServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pubbyserver/PubbyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pubbyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubbyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
